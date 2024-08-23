55190914f9351972bd7596f0cd37163ab7e87495
Shota Iwamoto <amaginqoo@gmail.com>
2024-08-22 18:47:00 -0700

    Add Home list page (#2)

--- RN/components/ThemedView.tsx
+++ RN/components/ThemedView.tsx
@@ -1,14 +1,31 @@
-import { View, type ViewProps } from 'react-native';
+import { View, type ViewProps } from "react-native";
 
-import { useThemeColor } from '@/hooks/useThemeColor';
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
+  wrapper?: boolean;
 };
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+export function ThemedView({
+  style,
+  lightColor,
+  darkColor,
+  wrapper,
+  ...otherProps
+}: ThemedViewProps) {
+  const backgroundColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "background"
+  );
+
+  if (wrapper) {
+    return (
+      <SafeAreaView style={[{ backgroundColor }, style]} {...otherProps} />
+    );
+  }
 
   return <View style={[{ backgroundColor }, style]} {...otherProps} />;
 }
