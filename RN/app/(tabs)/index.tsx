bc57c94d32d20da92c346d708f39b43399f3bf43
Ryo Togashi <ryotogashi@Ryos-MacBook-Pro.local>
2024-08-26 21:28:01 -0700

    implement useGetQRCodeFiles

--- RN/app/(tabs)/index.tsx
+++ RN/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, TouchableOpacity, FlatList, Text } from "react-native";
 import { useShareIntent } from "expo-share-intent";
-import { uploadQRcodeFile } from "@/repositories/FileSystem/uploadQRcodeFile";
+import { useGetQRCodeFiles } from "@/hooks/useGetQRcodeFiles";
 
 // Components
 import { ThemedText } from "@/components/ThemedText";
@@ -8,8 +8,6 @@ import { ThemedView } from "@/components/ThemedView";
 // Icons
 import AntDesign from "@expo/vector-icons/AntDesign";
 import Entypo from "@expo/vector-icons/Entypo";
-import { useEffect, useState } from "react";
-import { getQRcodeFiles } from "@/repositories/FileSystem/getQRcodeFiles";
 
 type FileType = {
   name: string;
@@ -20,22 +18,7 @@ export default function HomeScreen() {
   const { hasShareIntent, shareIntent, resetShareIntent, error } =
     useShareIntent();
 
-  const [files, setFiles] = useState<FileType[]>([]);
-
-  useEffect(() => {
-    (async () => {
-      const _files = await getQRcodeFiles();
-      const typedFiles = _files.map(
-        (file) =>
-          ({
-            name: file.name,
-            path: file.path,
-          }) as FileType
-      );
-
-      setFiles(typedFiles);
-    })();
-  }, []);
+  const { files } = useGetQRCodeFiles();
 
   const renderItem = ({ item }: { item: FileType }) => {
     return (
@@ -49,32 +32,8 @@ export default function HomeScreen() {
   };
 
   async function plusButtonClick() {
-    const test = await uploadQRcodeFile({
-      name: "cat_file.jpg",
-      filepath:
-        "/Users/ryotogashi/Library/Developer/CoreSimulator/Devices/AEFA1D5E-67B7-4632-959F-1139860C27E3/data/Media/DCIM/100APPLE/IMG_0001.JPG",
-      filetype: "jpg",
-    });
-
-    console.log(test);
-
     alert("Add new item");
   }
-
-  const onGetImages = async () => {
-    const _files = await getQRcodeFiles();
-    const typedFiles = _files.map(
-      (file) =>
-        ({
-          name: file.name,
-          path: file.path,
-        }) as FileType
-    );
-
-    console.log(typedFiles);
-
-    setFiles(typedFiles);
-  };
 
   return (
     <ThemedView wrapper style={styles.wrapper}>
@@ -83,9 +42,6 @@ export default function HomeScreen() {
       <FlatList data={files} renderItem={renderItem} />
       <TouchableOpacity onPress={plusButtonClick} style={styles.plusIcon}>
         <AntDesign name="pluscircle" size={48} color="black" />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={onGetImages} style={styles.getImageBtn}>
-        <AntDesign name="pluscircle" size={48} color="red" />
       </TouchableOpacity>
     </ThemedView>
   );
@@ -122,10 +78,5 @@ const styles = StyleSheet.create({
     position: "absolute",
     bottom: 28,
     right: 40,
-  },
-  getImageBtn: {
-    position: "absolute",
-    bottom: 28,
-    left: 40,
   },
 });
