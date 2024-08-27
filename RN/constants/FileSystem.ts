f9e2ed0f767cf4e3bc4a5597c934dc38947f026c
Ryo Togashi <ryotogashi@Ryos-MacBook-Pro.local>
2024-08-26 21:12:46 -0700

    Fix FileSystem functions

--- RN/constants/FileSystem.ts
+++ RN/constants/FileSystem.ts
@@ -1,8 +1,4 @@
 import * as RNFS from "react-native-fs";
-import * as path from "path";
 
-export const QR_CODALIZE_IMAGE_DIR = "QRCodalize";
-export const ROOT_DIR_FILE_PATH = path.join(
-  RNFS.MainBundlePath,
-  QR_CODALIZE_IMAGE_DIR
-);
+export const QR_CODALIZE_IMAGE_DIR = "QRCodalizeAssets";
+export const ROOT_DIR_FILE_PATH = `${RNFS.MainBundlePath}/${QR_CODALIZE_IMAGE_DIR}`;
