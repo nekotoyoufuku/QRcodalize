333b6ef48180131eabd85008bf5bc60d253c48fc
Ryo Togashi <ryotogashi@Ryos-MacBook-Pro.local>
2024-09-01 15:32:26 -0700

    change tab explore to playground

---
+++ RN/app/(tabs)/playground.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { NewQRCode } from "@/components/NewQRCode";
+
+export default function Playground() {
+  return (
+    <ThemedView wrapper style={styles.wrapper}>
+      <ThemedView style={styles.header}>
+        <ThemedText type="title">Playground</ThemedText>
+      </ThemedView>
+      {/* Playground here */}
+
+      {/* Playground here */}
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    marginBottom: 40,
+  },
+  wrapper: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+  },
+});
