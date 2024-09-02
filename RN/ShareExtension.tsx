c4c385441e2ca76109fc9e866fe6e7e10da6ea59
Shota Iwamoto <amaginqoo@gmail.com>
2024-09-02 16:39:21 -0700

    update ShareExtension.tsx remove customComponent

--- RN/ShareExtension.tsx
+++ RN/ShareExtension.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { type InitialProps, close } from "expo-share-extension";
 
-import { createQRCodeFile } from "@/repositories/FileSystem/createQRCodeFile";
+// import { createQRCodeFile } from "@/repositories/FileSystem/createQRCodeFile";
 // import Button from "@/components/Button/Button";
 // import TextInputField from "@/components/TextInput";
 
@@ -10,15 +10,15 @@ export default function ShareExtension({ images }: InitialProps) {
   const [text, onChangeText] = React.useState("test_file");
 
   const handleOpenHostApp = async () => {
-    await createQRCodeFile({
-      name: text,
-      filepath: images![0],
-    });
+    //   await createQRCodeFile({
+    //     name: text,
+    //     filepath: images![0],
+    //   });
 
-    // When you share images and videos, expo-share-extension stores them in a sharedData
-    // directory in your app group's container. These files are not automatically cleaned up,
-    // so you should delete them when you're done with them.
-    // await clearAppGroupContainer()
+    //   // When you share images and videos, expo-share-extension stores them in a sharedData
+    //   // directory in your app group's container. These files are not automatically cleaned up,
+    //   // so you should delete them when you're done with them.
+    //   // await clearAppGroupContainer()
     close();
   };
 
@@ -30,6 +30,13 @@ export default function ShareExtension({ images }: InitialProps) {
         Media Example
       </Text>
 
+      <TouchableOpacity onPress={handleOpenHostApp}>
+        <Text
+          style={{ fontFamily: "Inter-Black", fontSize: 24, marginBottom: 10 }}
+        >
+          Close
+        </Text>
+      </TouchableOpacity>
       {/* <TextInputField value={text} onChangeText={onChangeText} />
       <View style={styles.primaryButtonWrapper}>
         <Button title="Save" onPress={handleOpenHostApp} state="default" />
