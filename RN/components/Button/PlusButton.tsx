dd9a9b2e5f12fd4422b990087c4bae115d03ffd2
Ryo Togashi <35892492+ryonryon@users.noreply.github.com>
2024-09-07 16:05:03 -0700

    Fix plus button (#33)
    Co-authored-by: Ryo Togashi <ryotogashi@Ryos-MacBook-Pro.local>

--- RN/components/Button/PlusButton.tsx
+++ RN/components/Button/PlusButton.tsx
@@ -1,17 +1,43 @@
-import { StyleSheet, TouchableOpacity } from "react-native";
-
-// Icons
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import AntDesign from "@expo/vector-icons/AntDesign";
 
-export function PlusButton({ onPress }: { onPress?: () => void }) {
+import { useThemeColor } from "@/hooks/useThemeColor";
+
+export function PlusButton({
+  onPress,
+  lightColor,
+  darkColor,
+}: {
+  onPress?: () => void;
+  lightColor?: string;
+  darkColor?: string;
+}) {
+  const color = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "plusIcon"
+  );
+  const backgroundColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "plusIconBackgound"
+  );
+
   return (
     <TouchableOpacity onPress={onPress} style={styles.plusIcon}>
-      <AntDesign name="pluscircle" size={48} color="black" />
+      <View style={[styles.circle, { backgroundColor }]}>
+        <AntDesign name="plus" size={32} color={color} />
+      </View>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
+  circle: {
+    height: 48,
+    width: 48,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 24,
+  },
   plusIcon: {
     position: "absolute",
     bottom: 60,
