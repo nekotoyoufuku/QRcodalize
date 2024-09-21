783ef75dc9a8d1e349a53353328558c009de0114
Ryo Togashi <35892492+ryonryon@users.noreply.github.com>
2024-09-21 16:41:58 -0700

    implement add to wallet button (#52)
    Co-authored-by: Ryo Togashi <ryotogashi@Ryos-MacBook-Pro.local>

---
+++ RN/repositories/async/createEncodedPkpass.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+type ApiResponse = {
+  success: boolean;
+  pkpass: string;
+};
+
+export interface CreateEncodedPkpassInput {
+  pkpassName: string;
+}
+
+export async function createEncodedPkpass({
+  pkpassName,
+}: CreateEncodedPkpassInput): Promise<string> {
+  const url = `${process.env.BACKEND_URL}/create-encoded-pkpass/${pkpassName}.pkpass`;
+
+  try {
+    const response = await axios<ApiResponse>(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    if (!response.data.success) {
+      throw new Error(`Failed to get pkpass. Status code: ${response.status}`);
+    }
+
+    return response.data.pkpass;
+  } catch (error) {
+    throw new Error(error as any);
+  }
+}
